31b82a8a3836e5306d4ad21e45d2086bcf2b1725
luis2 <luis2@LAPTOP-LP>
2024-03-20 18:29:43 -0600

    Agregar archivos de proyecto.

---
+++ 202109710_tarea07/Module1.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+    Sub Main()
+        Const Rows As Integer = 2
+        Const Cols As Integer = 2
+        Dim A(Rows - 1, Cols - 1) As Integer
+        Console.WriteLine("Ingresar elementos de la matriz A:")
+        For i As Integer = 0 To Rows - 1
+            For j As Integer = 0 To Cols - 1
+                Console.Write($"Ingrese el elemento [{i},{j}]: ")
+                A(i, j) = Convert.ToInt32(Console.ReadLine())
+            Next
+        Next
+        Console.Clear()
+        Console.WriteLine("Matriz A:")
+        For i As Integer = 0 To Cols - 1
+            For j As Integer = 0 To Rows - 1
+                Console.Write($"{A(i, j)} ")
+            Next
+            Console.WriteLine()
+        Next
+        Console.WriteLine("Matriz transpuesta de A:")
+        For i As Integer = 0 To Cols - 1
+            For j As Integer = 0 To Rows - 1
+                Console.Write($"{A(j, i)} ")
+            Next
+            Console.WriteLine()
+        Next
+        Console.ReadLine()
+    End Sub
+End Module
